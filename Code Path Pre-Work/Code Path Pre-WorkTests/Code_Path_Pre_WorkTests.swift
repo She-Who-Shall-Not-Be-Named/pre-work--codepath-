9810a7b40e3858186d1e671db5be0e8bb4281777
She-Who-Shall-Not-Be-Named <chanellesactive@gmail.com>
2021-08-09 20:42:40 -0400

    readme

---
+++ Code Path Pre-Work/Code Path Pre-WorkTests/Code_Path_Pre_WorkTests.swift
@@ -0,0 +1,33 @@
+//
+//  Code_Path_Pre_WorkTests.swift
+//  Code Path Pre-WorkTests
+//
+//  Created by Alicia Isler on 8/3/21.
+//
+
+import XCTest
+@testable import Code_Path_Pre_Work
+
+class Code_Path_Pre_WorkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
